280340a1b33a074bd7fd4fcd1a6f9b8d646f0971
heetae185 <50399416+heetae185@users.noreply.github.com>
2022-05-26 13:31:15 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen! Let's test this one!</h2>
+      <h2>
+        Kimhyungbin Start editing to see some magic happen! Let's test this one!
+      </h2>
     </div>
   );
 }
