45e79fb7fd0fb1ee813599df1dfb344cec30ee19
HyeBeen <86273626+ne0n9uy@users.noreply.github.com>
2022-06-02 02:00:04 +0900

    initial commit

--- src/LeftTop.js
+++ src/LeftTop.js
@@ -6,13 +6,13 @@ import flag from "./imgs/Q.jpeg";
 function LeftTop(props) {
   return (
     <>
-      <div class="left_up">
+      <div className="left_up">
         <div className="LeftTop1">
-          <div class="flag">
+          <div className="flag">
             <img width="100%" height="100%" src={flag} alt="flag img" />
           </div>
 
-          <div class="Rank">
+          <div className="Rank">
             <b>
               <h2 id="Rank">FIFA RANK</h2>
               <h1>{props.Rank}</h1>
@@ -21,13 +21,13 @@ function LeftTop(props) {
         </div>
 
         <div className="LeftTop2">
-          <div class="Nat_name">
+          <div className="Nat_name">
             <b>
               <h1 id="Nat_name">{props.Nat_name}</h1>
             </b>
           </div>
 
-          <div class="Pred_rank">
+          <div className="Pred_rank">
             <b>
               <h2 id="Pred_rank">Predicted RANK</h2>
               <h1>{props.Pred_rank}</h1>
